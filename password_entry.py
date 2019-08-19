c88ce4435baf516c0a748add6b16b3980ec387dc
zacmarch <zacmarch101@gmail.com>
2019-08-20 09:46:19 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 """Zac March"""
+MIN_LENGTH = 5
+
+password = input("Enter password: ")
+while len(password) < MIN_LENGTH:
+    print("Password must be {} characters long".format(MIN_LENGTH))
+    password = input("Enter password: ")
+output = ""
+for char in password:
+    output += "*"
+print(output)
+
